fd5d675cc16b7fefc2561e0b83354b28d67f0ba2
Giuseppe Rota <rota.giuseppe@gmail.com>
2014-03-22 16:29:48 +0100

    update tmux script

--- tmux.sh
+++ tmux.sh
@@ -4,8 +4,12 @@ tmux has-session -t "$SESSION_NAME" 2> /dev/null
 if [ $? != 0  ]; then
   # -d detaches, -n is for name
   tmux new-session -s "$SESSION_NAME" -d -n "shell"
-  tmux split-window -h -t "$SESSION_NAME" 'vim'
+  tmux split-window -h -t "$SESSION_NAME"
+  tmux send-keys "vim app.js"
   tmux select-pane -L
   tmux split-window -v
+  tmux select-pane -R
+  sleep 1
+  tmux send-keys C-m
 fi
 tmux attach -t "$SESSION_NAME"

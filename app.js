0b3288bf35a8ed1b83200a541b539052d00a6622
Giuseppe Rota <rota.giuseppe@gmail.com>
2014-03-23 10:49:21 +0100

    Step 2, baby steps

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-console.log('HELLO WORLD');
+var argv = process.argv;
+argv.shift(); argv.shift();
+console.log(argv.reduce(function(prev, current, index, array) {
+  return Number(prev)+Number(current);
+}));

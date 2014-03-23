11ba8590949de15d43eb6c0ecc6cc3b6373923c6
Giuseppe Rota <rota.giuseppe@gmail.com>
2014-03-23 11:15:59 +0100

    step 4 with path module

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 var fs = require('fs')
+var path = require('path')
 var dir = process.argv[2];
 var ext = process.argv[3];
 fs.readdir(dir, function (err, files) {
   var filtered_list = files.filter(function(elem){
-    var parts = elem.split('.');
-    if (parts.length < 2) return false;
-    var file_ext = parts.pop();
-    return file_ext === ext;
+    var file_ext = path.extname(elem);
+    return file_ext === '.'+ext;
   });
   console.log(filtered_list.join('\n'));
 });

ecddc09413e7c41af56d7e1b6467bd5aa0c76d88
Giuseppe Rota <rota.giuseppe@gmail.com>
2014-03-23 23:14:54 +0100

    Juggling async

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 var http = require('http')
 var bl = require('bl')
-var url = process.argv[2];
-http.get(url, function(res) {
-  res.pipe(bl( function(err, data) {
-    console.log( data.length);
-    console.log( data.toString());
-  } ));
-});
+var arrived = [];
+var total = 0;
+
+for (var i=0; i < 3; i++) {
+  mothereffingjs(i);
+}
+
+function mothereffingjs(i) {
+  var url = process.argv[2+i];
+  http.get(url, function(res) {
+    res.pipe(bl( function(err, data) {
+      if (err) {
+        return console.log(data);
+      }
+      arrived[i] = data.toString();
+      total++;
+      if (total == 3) {
+        for (var j=0; j < 3; j++) {
+          console.log(arrived[j]);
+        }
+      }
+    } ));
+  });
+}

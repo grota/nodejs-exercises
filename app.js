683da4498ff44f486df51fcc94c88db207a2a008
Giuseppe Rota <rota.giuseppe@gmail.com>
2014-03-23 11:02:23 +0100

    step 4 first async

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var fs = require('fs')
 var fname = process.argv[2];
-var contents = fs.readFileSync(fname);
-var lines = contents.toString().split('\n');
-console.log(lines.length-1);
+fs.readFile(fname, {encoding: 'utf8'}, function (err, data) {
+  var lines = data.split('\n');
+  console.log(lines.length-1);
+});

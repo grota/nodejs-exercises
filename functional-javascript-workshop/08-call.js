6e991baca98a3662c74da0b996254fe0404dc839
Giuseppe Rota <rota.giuseppe@gmail.com>
2014-03-30 19:05:48 +0200

    08-call

---
+++ functional-javascript-workshop/08-call.js
@@ -0,0 +1,6 @@
+module.exports = duckCount;
+function duckCount () {
+  return Array.prototype.slice.call(arguments).filter(function(obj) {
+    return Object.prototype.hasOwnProperty.call(obj, 'quack')
+  }).length;
+}
